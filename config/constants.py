0058329cdfec0b2d2beb56da80232d00f4826111
KhanTaufeeq <taufeeqyouth@gmail.com>
2024-12-25 11:24:02 +0530

    updated backend

---
+++ config/constants.py
@@ -0,0 +1,24 @@
+##############################
+# Transaction
+##############################
+TRANSACTION_TYPE = (
+    ('income', 'Income'),
+    ('expense', 'Expense'),
+)
+TRANSACTION_TYPE_DICT = dict(TRANSACTION_TYPE)
+
+#############################
+# Category 
+#############################
+CATEGORY_TYPE = (
+  ('utitlity', 'Utility'),
+  ('food', 'Food'),
+  ('education', 'Education'),
+  ('transportation', 'Transportation'),
+  ('medical', 'Medical'),
+  ('internet & tv', 'Internet & TV'),
+  ('entertainment', 'Entertainment'),
+  ('salary', 'Salary'),
+  ('other', 'Other')
+)
+CATEGORY_TYPE_DICT = dict(CATEGORY_TYPE)
